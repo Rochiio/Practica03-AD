32f2d6137e9653c21631e4e2dcb5cd6e00dcf25d
Moha <moha.as172@gmail.com>
2023-01-16 01:46:18 +0100

    repository tasks

---
+++ TennisLabMongoDB/src/main/kotlin/repositories/orders/TaskRepositoryImpl.kt
@@ -0,0 +1,46 @@
+package repositories.orders
+
+import db.MongoDbManager
+import kotlinx.coroutines.flow.Flow
+import model.orders.tasks.Task
+import mu.KotlinLogging
+import org.litote.kmongo.setTo
+import java.util.*
+
+class TaskRepositoryImpl : TaskRepository {
+    private var logger = KotlinLogging.logger{}
+    private  var dbMongo = MongoDbManager.database
+    override suspend fun findById(id: UUID): Task? {
+        logger.debug { "Buscando tarea con id: $id" }
+        return dbMongo.getCollection<Task>().findOneById(id)
+    }
+
+    override suspend fun save(item: Task): Task {
+        logger.debug { "Guardando tarea: $item" }
+        return dbMongo.getCollection<Task>()
+            .save(item).let { item }
+    }
+
+    override suspend fun update(item: Task): Task {
+        logger.debug { "Actualizando tarea: $item" }
+        return  dbMongo.getCollection<Task>()
+            .updateOneById(item.id, item)
+            .wasAcknowledged().let { item }
+    }
+
+    override suspend fun delete(item: Task): Boolean {
+        logger.debug { "Eliminando tarea: $item" }
+        return dbMongo.getCollection<Task>()
+            .updateOneById(item.id, Task::available.setTo(false)).wasAcknowledged()
+    }
+
+    override suspend fun findAll(): Flow<Task> {
+        logger.debug { "Recuperando todas las tareas "}
+        return dbMongo.getCollection<Task>().find().toFlow()
+    }
+
+    override suspend fun deleteAll(): Boolean {
+        logger.debug { "Eliminando todas las tareas" }
+        return dbMongo.getCollection<Task>().deleteMany("{}").wasAcknowledged()
+    }
+}
