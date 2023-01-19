f118a463f6aeab5757086b2f506e43cf31ad5e65
Rocio <rpalaofernandez@gmail.com>
2023-01-19 22:07:49 +0100

    nuevo proyecto, parece q ya funciona hacer test :))

---
+++ TennisLabMongo/src/main/kotlin/model/orders/Order.kt
@@ -0,0 +1,30 @@
+package model.orders
+
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.Serializable
+import model.Status
+import model.orders.tasks.Task
+import model.users.Customer
+import org.bson.codecs.pojo.annotations.BsonId
+import org.litote.kmongo.Id
+import util.serializer.LocalDateSerializer
+import java.time.LocalDate
+
+@Serializable
+data class Order(
+    @BsonId @Contextual
+    var id: Id<Order>,
+    var state: Status,
+    @Serializable(with = LocalDateSerializer::class)
+    val entryDate: LocalDate?,
+    @Serializable(with = LocalDateSerializer::class)
+    var exitDate: LocalDate?,
+    @Serializable(with = LocalDateSerializer::class)
+    var finalDate: LocalDate?,
+    @Serializable(with = LocalDateSerializer::class)
+    val maxDate: LocalDate?,
+    var totalPrice: Float,
+    val client: Customer,
+    var tasks: ArrayList<Task>
+)
+
