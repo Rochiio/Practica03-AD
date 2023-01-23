e7ce0c19e48179f62491da1df014e673da01e7df
Mohamed Asidah Bchiri <moha.as172@gmail.com>
2023-01-23 19:03:59 +0100

    commit

---
+++ TennisLabMongoDB/src/main/kotlin/services/api/ApiTasks.kt
@@ -0,0 +1,26 @@
+package services.api
+
+import de.jensklingenberg.ktorfit.http.*
+import dto.TaskDTO
+
+interface ApiTasks {
+
+    @GET("todos")
+    suspend fun getAll(@Query("page") page: Int = 0, @Query("per_page") perPage: Int = 0): List<TaskDTO>
+
+    @GET("todos/{id}")
+    suspend fun getById(@Path("id") id: Int): TaskDTO?
+
+    @GET("todos/{userId}")
+    suspend fun getByUserId(@Path("userId") userId: Int): TaskDTO?
+
+    @POST("todos")
+    suspend fun create(@Body task: TaskDTO): TaskDTO
+
+    @PUT("todos/{id}")
+    suspend fun update(@Path("id") id: Int, @Body task: TaskDTO): TaskDTO
+
+    @DELETE("todos/{id}")
+    suspend fun delete(@Path("id") id: Int)
+
+}
