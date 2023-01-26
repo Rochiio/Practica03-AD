46eac7d92a3750c97ad63e95b9fcf314166c2080
Rocio <rpalaofernandez@gmail.com>
2023-01-26 18:21:10 +0100

    json order

--- TennisLabMongo/src/main/kotlin/service/files/JsonFilesOrder.kt
+++ TennisLabMongo/src/main/kotlin/service/files/JsonFilesOrder.kt
@@ -4,24 +4,23 @@ import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
-import model.lists.StringerAssignmentsList
+import model.orders.Order
 import java.io.File
 import java.io.FileOutputStream
 import java.io.OutputStreamWriter
-
 /**
- * Escribir el fichero de asignaciones en json.
+ * Escribir el fichero de pedido en json.
  */
-class JsonFilesAssignments: JsonFiles<StringerAssignmentsList> {
+class JsonFilesOrder: JsonFiles<Order> {
     private var json = Json { prettyPrint=true }
     private var mutex = Mutex()
 
-    override suspend fun writeFichero(path: String, item: StringerAssignmentsList) {
+    override suspend fun writeFichero(path: String, item: Order) {
         var out: OutputStreamWriter
         var fichero = File(path)
 
         mutex.withLock {
-            out = OutputStreamWriter(FileOutputStream(fichero, true))
+            out = OutputStreamWriter(FileOutputStream(fichero, false))
             out.use { it.write(json.encodeToString(item)) }
         }
     }
