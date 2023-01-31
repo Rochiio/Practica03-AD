6ccb5911521ecde964a0d67bcd0b2328ed7cf446
Rocio <rpalaofernandez@gmail.com>
2023-01-31 16:08:07 +0100

    cambio main

--- TennisLabMongo/src/main/kotlin/util/Data.kt
+++ TennisLabMongo/src/main/kotlin/util/Data.kt
@@ -3,11 +3,13 @@ package util
 import model.lists.StringerAssignments
 import model.orders.Order
 import model.orders.tasks.Task
+import java.io.File
 
 /**
  * Datos necesarios para realizar distintas acciones.
  */
 object Data {
+    val DIR_JSON = System.getProperty("user.dir") + File.separator + "listados" + File.separator
     var assignments= mutableListOf<StringerAssignments>()
     var completeOrders = mutableListOf<Order>()
     var pendingOrders = mutableListOf<Order>()
